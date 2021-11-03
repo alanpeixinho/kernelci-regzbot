7a65a7a88548ae076270483c4da1bf305dd9ca1b
Thorsten Leemhuis <linux@leemhuis.info>
2021-11-03 14:12:56 +0100

    web: s/oldest/earliest/
    Signed-off-by: Thorsten Leemhuis <linux@leemhuis.info>

--- regzbot/export_web.py
+++ regzbot/export_web.py
@@ -112,7 +112,7 @@ class RegressionWeb(regzbot.RegressionFull):
                         if len(self._actievents) < 2:
                             yattagdoc.text('Reported: %s; no further activity yet.' % yattagdoc.text('%s' % days_delta(self._actievents[0].gmtime)))
                         else:
-                            yattagdoc.text('Oldest and latest activity: ')
+                            yattagdoc.text('Earliest and latest activity: ')
                             with yattagdoc.tag('a', href=self._actievents[0].url()):
                                 yattagdoc.text('%s' % days_delta(
                                     self._actievents[0].gmtime))
