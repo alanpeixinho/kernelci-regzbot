3fbd1afb22b06995667d804f2be87127cdf44168
Thorsten Leemhuis <linux@leemhuis.info>
2022-07-06 09:31:40 +0200

    startscript: allow testing bugzilla code
    Signed-off-by: Thorsten Leemhuis <linux@leemhuis.info>

--- regzbot.sh
+++ regzbot.sh
@@ -6,4 +6,10 @@
 SCRIPT=$(realpath -e ${BASH_SOURCE[0]})
 SCRIPT_TOP="$(dirname ${SCRIPT})"
 
-exec env PYTHONPATH="${SCRIPT_TOP}" python3 "${SCRIPT_TOP}/regzbot/commandl.py" "${@}"
+if [[ -e "$(dirname "${0}")/regzbot/${1}" ]]; then
+    toexecute="${1}"
+    shift
+    exec env PYTHONPATH="${SCRIPT_TOP}" python3 "${SCRIPT_TOP}/regzbot/${toexecute}" "${@}"
+else
+    exec env PYTHONPATH="${SCRIPT_TOP}" python3 "${SCRIPT_TOP}/regzbot/commandl.py" "${@}"
+fi
