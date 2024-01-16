5aa961717ca8fefad6136f626fee10b07d6a8b3d
Thorsten Leemhuis <linux@leemhuis.info>
2024-01-16 12:15:23 +0100

    lore: code movent, no changes
    Signed-off-by: Thorsten Leemhuis <linux@leemhuis.info>

--- regzbot/_repsources/_lore.py
+++ regzbot/_repsources/_lore.py
@@ -40,6 +40,111 @@ class LoreDownloadError(Exception):
     pass
 
 
+class LoreNntp():
+    # without this, we occasionally [as on 20210831] run into
+    # "nntplib.NNTPDataError: line too long" errors
+    # might be a bug in the public-inbox code behind lore
+    nntplib._MAXLINE = 65536
+
+    def __init__(self):
+        global _NNTP_CONNECTION
+        if _NNTP_CONNECTION == None:
+            logger.debug('connecting to nntp.lore.kernel.org')
+            _NNTP_CONNECTION = nntplib.NNTP('nntp.lore.kernel.org')
+        self._nntp_connection = _NNTP_CONNECTION
+
+    def _article(self, id):
+        if isinstance(id, str) and id[0] != '<':
+            id = '<%s>' % id
+        _, article = self._nntp_connection.article(id)
+        return email.message_from_bytes(b'\n'.join(article.lines), policy=email.policy.default)
+
+    def _group(self, groupname):
+        splitted  = groupname.split('/', maxsplit=4)
+        if len(splitted)>2:
+            groupname = splitted[3]
+        else:
+            groupname = splitted[0]
+        logger.debug('opening group %s', groupname)
+        _, _, id_first, id_last, _ = self._nntp_connection.group(groupname)
+        return id_first, id_last
+
+    def _over(self, id_first, id_last):
+        _, overviews = self._nntp_connection.over((id_first, id_last))
+        for id, over in overviews:
+            yield id, over
+
+    def update(self):
+        for repsrc in regzbot.ReportSource.getall_bykind('lore'):
+            id_first, id_last = self._group(groupname)
+
+            if not repsrc.lastchked:
+                repsrc.set_lastchked(id_first)
+                logger.info(
+                    'seeing %s for the first time, starting to monitor it from now on', repsrc.serverurl)
+                repsrc.set_lastchked(id_last)
+                continue
+            elif repsrc.lastchked == id_last:
+                logger.debug('nothing new in %s', repsrc.serverurl)
+                continue
+
+            logger.debug('processing "%s"', repsrc.serverurl)
+            for id, over in self._over(repsrc.lastchked + 1, id_last):
+                msgid = regzbot.mailin.email_get_msgid(over['message-id'])
+                gmtime = email.utils.mktime_tz(email.utils.parsedate_tz(over['date']))
+                if regzbot.RecordProcessedMsgids.check_presence(msgid, gmtime):
+                   logger.debug('[lore] skipping "%s", we already encountered it it', msgid)
+                   continue
+
+                article = self._article(id)
+                msg = email.message_from_bytes(b'\n'.join(article.lines), policy=policy.default)
+                regzbot.mailin.process_msg(repsrc, msg)
+
+            # update database
+            repsrc.set_lastchked(id_last)
+            regzbot.db_commit()
+
+
+class LoreHttps():
+    @staticmethod
+    def download_thread(msgid, repsrcid = None):
+        with tempfile.NamedTemporaryFile() as tmpfile:
+            url='https://lore.kernel.org/all/%s/t.mbox.gz' % msgid
+            try:
+                logger.debug("[lore] downloading %s", url)
+                with urllib.request.urlopen(url) as response:
+                    with gzip.open(response) as uncompressed:
+                        shutil.copyfileobj(uncompressed, tmpfile)
+            except urllib.error.HTTPError as err:
+                logger.critical('[lore] failed to download thread from %s: %s', url, err)
+                raise LoreDownloadError()
+            for message in mailbox.mbox(tmpfile.name):
+                yield email.message_from_bytes(message.as_bytes(), policy=email.policy.default)
+
+    @staticmethod
+    def download_msg(msgid):
+        with tempfile.NamedTemporaryFile() as tmpfile:
+            url='https://lore.kernel.org/all/%s/raw' % msgid
+            try:
+                logger.debug("[lore] downloading %s", url)
+                with urllib.request.urlopen(url) as response:
+                    shutil.copyfileobj(response, tmpfile)
+                    return True
+            except urllib.error.HTTPError as err:
+                logger.warning('[lore] could not download msg %s: %s"', msgid, err)
+                raise LoreDownloadError()
+
+            # result might contain a raw msg or a mbox file with multiple messages
+            mbox = mailbox.mbox(tmpfile.name)
+            if mbox:
+                for message in mbox:
+                     # just pick the first one
+                     return email.message_from_bytes(message.as_bytes(), policy=email.policy.default)
+            else:
+                tmpfile.seek(0)
+                return email.message_from_string(tmpfile.read().decode('utf-8', errors='ignore'), policy=email.policy.default)
+
+
 class LoActivity():
     def __init__(self, msg):
         self._msg = msg
@@ -260,110 +365,6 @@ class LoreThread():
             elif until and activity.created_at > until:
                 continue
             yield activity
-
-class LoreNntp():
-    # without this, we occasionally [as on 20210831] run into
-    # "nntplib.NNTPDataError: line too long" errors
-    # might be a bug in the public-inbox code behind lore
-    nntplib._MAXLINE = 65536
-
-    def __init__(self):
-        global _NNTP_CONNECTION
-        if _NNTP_CONNECTION == None:
-            logger.debug('connecting to nntp.lore.kernel.org')
-            _NNTP_CONNECTION = nntplib.NNTP('nntp.lore.kernel.org')
-        self._nntp_connection = _NNTP_CONNECTION
-
-    def _article(self, id):
-        if isinstance(id, str) and id[0] != '<':
-            id = '<%s>' % id
-        _, article = self._nntp_connection.article(id)
-        return email.message_from_bytes(b'\n'.join(article.lines), policy=email.policy.default)
-
-    def _group(self, groupname):
-        splitted  = groupname.split('/', maxsplit=4)
-        if len(splitted)>2:
-            groupname = splitted[3]
-        else:
-            groupname = splitted[0]
-        logger.debug('opening group %s', groupname)
-        _, _, id_first, id_last, _ = self._nntp_connection.group(groupname)
-        return id_first, id_last
-
-    def _over(self, id_first, id_last):
-        _, overviews = self._nntp_connection.over((id_first, id_last))
-        for id, over in overviews:
-            yield id, over
-
-    def update(self):
-        for repsrc in regzbot.ReportSource.getall_bykind('lore'):
-            id_first, id_last = self._group(groupname)
-
-            if not repsrc.lastchked:
-                repsrc.set_lastchked(id_first)
-                logger.info(
-                    'seeing %s for the first time, starting to monitor it from now on', repsrc.serverurl)
-                repsrc.set_lastchked(id_last)
-                continue
-            elif repsrc.lastchked == id_last:
-                logger.debug('nothing new in %s', repsrc.serverurl)
-                continue
-
-            logger.debug('processing "%s"', repsrc.serverurl)
-            for id, over in self._over(repsrc.lastchked + 1, id_last):
-                msgid = regzbot.mailin.email_get_msgid(over['message-id'])
-                gmtime = email.utils.mktime_tz(email.utils.parsedate_tz(over['date']))
-                if regzbot.RecordProcessedMsgids.check_presence(msgid, gmtime):
-                   logger.debug('[lore] skipping "%s", we already encountered it it', msgid)
-                   continue
-
-                article = self._article(id)
-                msg = email.message_from_bytes(b'\n'.join(article.lines), policy=policy.default)
-                regzbot.mailin.process_msg(repsrc, msg)
-
-            # update database
-            repsrc.set_lastchked(id_last)
-            regzbot.db_commit()
-
-
-class LoreHttps():
-    @staticmethod
-    def download_thread(msgid, repsrcid = None):
-        with tempfile.NamedTemporaryFile() as tmpfile:
-            url='https://lore.kernel.org/all/%s/t.mbox.gz' % msgid
-            try:
-                logger.debug("[lore] downloading %s", url)
-                with urllib.request.urlopen(url) as response:
-                    with gzip.open(response) as uncompressed:
-                        shutil.copyfileobj(uncompressed, tmpfile)
-            except urllib.error.HTTPError as err:
-                logger.critical('[lore] failed to download thread from %s: %s', url, err)
-                raise LoreDownloadError()
-            for message in mailbox.mbox(tmpfile.name):
-                yield email.message_from_bytes(message.as_bytes(), policy=email.policy.default)
-
-    @staticmethod
-    def download_msg(msgid):
-        with tempfile.NamedTemporaryFile() as tmpfile:
-            url='https://lore.kernel.org/all/%s/raw' % msgid
-            try:
-                logger.debug("[lore] downloading %s", url)
-                with urllib.request.urlopen(url) as response:
-                    shutil.copyfileobj(response, tmpfile)
-                    return True
-            except urllib.error.HTTPError as err:
-                logger.warning('[lore] could not download msg %s: %s"', msgid, err)
-                raise LoreDownloadError()
-
-            # result might contain a raw msg or a mbox file with multiple messages
-            mbox = mailbox.mbox(tmpfile.name)
-            if mbox:
-                for message in mbox:
-                     # just pick the first one
-                     return email.message_from_bytes(message.as_bytes(), policy=email.policy.default)
-            else:
-                tmpfile.seek(0)
-                return email.message_from_string(tmpfile.read().decode('utf-8', errors='ignore'), policy=email.policy.default)
 
 
 class LoRepAct(regzbot.ReportActivity):
